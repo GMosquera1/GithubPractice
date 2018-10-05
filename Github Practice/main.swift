6abec5953b7e8cd479e3c0e69243bedc2650517d
GMosquera1 <genesismosquera@pursuit.org>
2018-10-05 15:18:24 -0400

    Initial Commit

---
+++ Github Practice/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Github Practice
+//
+//  Created by Genesis Mosquera on 10/5/18.
+//  Copyright © 2018 Genesis Mosquera. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
